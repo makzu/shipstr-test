d2732ea8d7cf703703343c6c3bdbcd8e43100af0
Bill Weepie <bill@billweepie.com>
2015-09-20 18:47:26 -0700

    Basic searching & displaying of data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    @hide_menu = true
+  end
+
+  def browse
+    @sections = Section.find(params[:section])
+  end
+
+  def search
+    lines = Line.where("description LIKE ?", "%#{params[:term]}%")
+    @line_ids = lines.map {|l| l.id }
+    @chapters = lines.map {|l| l.chapter }.uniq
+  end
+
+  def search_router
+    if params[:term].nil? or params[:term].empty?
+      redirect_to "/"
+    else
+      redirect_to "/search/#{params[:term]}#res"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'application#index'
+  get 'search/:term' => 'application#search'
+  get 's' => 'application#search_router'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
